d779843178dff5c200e8880c3d4c7f468fb0ca3b
Kay Krewson <kay.k@comcast.net>
2017-04-12 02:05:23 -0700

    removed users.js and references to it, updated index.html and index.hbs

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
